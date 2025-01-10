b312cfa6fcb622683e57f248636eeb044b891592
Christopher Olivier <cjolivier01@gmail.com>
2025-01-09 16:13:42 -0800

    install_deps

---
+++ install_deps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get install -y \
+  gnutls-dev \
+  libharfbuzz-dev \
+  libfribidi-dev \
+  libfreetype-dev \
+  libfontconfig-dev \
+  libflac-dev \
+  libx264-dev \
+  libx265-dev
+
